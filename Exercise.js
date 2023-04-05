427114d43cf411a2c9812261a295755adac62755
unknown <glebkalinichenkolol@gmail.com>
2023-04-05 11:00:45 +0300

    Tasks

---
+++ Exercise.js
@@ -0,0 +1,14 @@
+let firstNumber = prompt("Введіть перше число:");
+let secondNumber = prompt("Введіть друге число:");
+
+let plus = Number(firstNumber) + Number(secondNumber);
+alert(`Сума двох чисел: ${plus}`);
+
+let minus = Number(firstNumber) - Number(secondNumber);
+alert(`Різниця двох чисел: ${minus}`);
+
+let multiply = Number(firstNumber) * Number(secondNumber);
+alert(`Добуток двох чисел: ${multiply}`);
+
+let divide = Number(firstNumber) / Number(secondNumber);
+alert(`Ділення двох чисел: ${divide}`);
